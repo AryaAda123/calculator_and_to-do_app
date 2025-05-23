33ebec4382d10bfdd5134677973f8da936dd57b4
AryaAda123 <153177353+AryaAda123@users.noreply.github.com>
2025-05-23 05:04:07 -0700

    Update Calculator1.jsx

--- src/components/Calculator1.jsx
+++ src/components/Calculator1.jsx
@@ -88,6 +88,7 @@ const Calculator1 = () => {
   };
 
   return (
+    <div> Vishal Patel</div>
     <div className="calculator1-container">
       <h1>Calculator - Style 1</h1>
       <div className="calculator1">
